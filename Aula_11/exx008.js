119a58b04688e34dd9d785b2b0bcce8dbd78aa1a
Makumbani Eduardo Pedro <kandaeduardopedro395@gmail.com>
2024-07-15 14:35:04 +0100

    Condicões composta

---
+++ Aula_11/exx008.js
@@ -0,0 +1,7 @@
+var país = "Moçambique"
+console.log(`Vivendo em ${país}`)
+if (país == "Angola") { //condições composta
+    console.log(`você é Angolano!`)
+} else {
+    console.log(`Você é Estrangeiro!`)
+}
