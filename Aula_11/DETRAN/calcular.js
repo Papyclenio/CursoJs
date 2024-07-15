8da6ce3d4837459b7eb1221275dcdade62e1a473
Makumbani Eduardo Pedro <kandaeduardopedro395@gmail.com>
2024-07-15 18:24:51 +0100

    Conteudo JS agrecentei a estrutura condiçonal IF.

--- Aula_11/DETRAN/calcular.js
+++ Aula_11/DETRAN/calcular.js
@@ -2,7 +2,10 @@ function calcular() {
     var inpvel = document.getElementById(`inpvel`);
     var res = document.getElementsByClassName(`res`)[0];
     var vel = Number(inpvel.value);
-    res.innerHTML = `<p>Sua velocidade atual é de <b>${vel}km/h</b></p>`
+    res.innerHTML = `<p>Sua velocidade atual é de <b>${vel}km/h</b>.</p>`
+    if (vel > 60) {
+        res.innerHTML += `<p>Você está <b>Multado</b> por excesso de velocidade.</p>`
+    }
     res.innerHTML += `<p>Dirija sempre usando o cinto de segurança!</p>`
 
 }
