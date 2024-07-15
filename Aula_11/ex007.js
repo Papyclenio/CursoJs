5ca66e0d0f9b565c05e3de05e39d8d61c59575e6
Makumbani Eduardo Pedro <kandaeduardopedro395@gmail.com>
2024-07-15 14:18:48 +0100

    condição simples

---
+++ Aula_11/ex007.js
@@ -0,0 +1,6 @@
+var vel = 72.2 
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) { // condição simples
+    console.log(`Você utrapassou a velocidade permitido. MULTADO!`)
+}
+console.log(`Dirija sempre usando o sinto de segurança`)
