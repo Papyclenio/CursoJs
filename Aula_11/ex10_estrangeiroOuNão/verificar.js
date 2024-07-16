a5fbc93727872f012c88a0c5ec4072c7135a8136
Makumbani Eduardo Pedro <kandaeduardopedro395@gmail.com>
2024-07-16 01:19:20 +0100

    base com poblema

---
+++ Aula_11/ex10_estrangeiroOuNão/verificar.js
@@ -0,0 +1,7 @@
+function ver() {
+    var país = window.document.getElementById("txtpais");
+    var res = window.document.getElementById("rest");
+    var pais = Number("país.value")
+    res.innerHTML = país
+}
+    
